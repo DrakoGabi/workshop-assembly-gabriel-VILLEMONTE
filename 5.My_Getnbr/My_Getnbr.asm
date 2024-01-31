cd67f4a409d3ddd558db78086acbe61b52776154
DrakoGabi <gabriel.villemonte@epitech.eu>
2024-01-31 11:56:36 +0400

    get nbr segv

--- 5.My_Getnbr/My_Getnbr.asm
+++ 5.My_Getnbr/My_Getnbr.asm
@@ -2,4 +2,18 @@ section .text
     global my_Getnbr
 
 my_Getnbr:
+    init_get:
+        xor rax, rax
+        xor r8, r8
+        xor rcx, rcx
+        jmp loop_get
     
+    loop_get:
+    mov r8b, [rdi +rax]
+    mov rcx, r8b - r8b
+    inc rax
+    jne loop_get
+
+    end_loop:
+    ret
+
